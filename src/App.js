0c417023065f678a87dac71790e567a91fc1d85d
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-12-11 22:29:31 +0200

    Created Player component. Created hook to follow pressed key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Ground } from './components/Ground';
+import { Player } from './components/Player';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <ambientLight intensity={0.5} />
         <Physics>
           <Ground />
+          <Player />
         </Physics>
       </Canvas>
     </>
