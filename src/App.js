683c6d3543c88451103c0965ec855b6340113e02
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-12-11 23:37:31 +0200

    Added FPV

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
+import { FPV } from "./components/FPV"
 
 
 function App() {
@@ -11,11 +12,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[80,80,20]} />
         <ambientLight intensity={0.5} />
+        <FPV />
         <Physics>
           <Ground />
           <Player />
         </Physics>
       </Canvas>
+      <div className='absolute objectCenter'>+</div>
     </>
   );
 }
